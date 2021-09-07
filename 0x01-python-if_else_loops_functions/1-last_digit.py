380073e07a3792df866c7b24036a43b1559b977a
cristian oliveros <3308@holbertonschool.com>
2021-09-07 13:20:57 -0500

    added conditional in line 6

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if abs(number) % 10 > 5:
         print("Last digit of {} is -{} and is less than 6 and not 0"
               .format(number, abs(number) % 10))
 elif abs(number) % 10 == 0:
-    print("Last digit of {} is {} and is zero"
+    print("Last digit of {} is {} and is 0"
           .format(number, abs(number) % 10))
 elif abs(number) % 10 < 6 and number % 10 != 0:
     if number > 0:
