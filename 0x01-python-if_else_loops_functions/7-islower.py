eddea2330d342f07499285cd5eb24cedf2e2d8e0
cristian oliveros <3308@holbertonschool.com>
2021-09-07 14:28:42 -0500

    changes in loop

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(ord('a'), ord("z")):
+    for i in range(ord('a'), ord('z')):
         if c == chr(i):
             return (True)
     return (False)
