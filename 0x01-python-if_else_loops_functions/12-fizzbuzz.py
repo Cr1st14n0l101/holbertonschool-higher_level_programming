f097a642226e0a3721e47f439ce75ffcb3428a99
cristian oliveros <3308@holbertonschool.com>
2021-09-07 17:21:08 -0500

    modified the range to 101 in the loop in line 3

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
@@ -10,4 +10,4 @@ def fizzbuzz():
         elif i < 100:
             print("{} ".format(i), end="")
         elif i == 100:
-            print("{}".format(i), end="")
+            print("Buzz")
