ec1ce2d51c7760fa154112e859468f7f3482d113
cristian oliveros <3308@holbertonschool.com>
2021-09-07 13:02:32 -0500

    added abs function to number variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
+if abs(number) % 10 > 5:
     print("Last digit of {} is {} and is greather than 5"
-          .format(number, number % 10))
-elif number % 10 == 0:
+          .format(number, abs(number) % 10))
+elif abs(number) % 10 == 0:
     print("Last digit of {} is {} and is zero"
-          .format(number, number % 10))
-elif number % 10 < 6 and number % 10 != 0:
+          .format(number, abs(number) % 10))
+elif abs(number) % 10 < 6 and number % 10 != 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(number, number % 10))
+          .format(number, abs(number) % 10))
