c48db40491caa593c35fc1bad6907ff8f5de95d3
cristian oliveros <3308@holbertonschool.com>
2021-09-08 13:24:36 -0500

    added character : for lines 8 and 11

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if n == 1:
         print("0 arguments.")
     elif n == 2:
-        print("{} argument".format(n - 1))
+        print("{} argument:".format(n - 1))
         print("1: {}".format(sys.argv[1]))
     elif n > 1:
-        print("{} arguments".format(n - 1))
+        print("{} arguments:".format(n - 1))
         for i in range(1, n):
             print("{}: {}".format(i, sys.argv[i]))
