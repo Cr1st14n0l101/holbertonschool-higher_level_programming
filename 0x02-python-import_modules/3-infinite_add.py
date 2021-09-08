c749824dc2d127f38d30c32accc853c3e269a36a
cristian oliveros <3308@holbertonschool.com>
2021-09-08 14:31:41 -0500

    added task 3.

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    number = 0
+    lenght = len(sys.argv)
+
+    for i in range(1, lenght):
+        number += int(sys.argv[i])
+    print (number)
