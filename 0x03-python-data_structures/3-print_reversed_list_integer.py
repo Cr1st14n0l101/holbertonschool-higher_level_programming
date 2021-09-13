8f67b9214e8a7cfaad2520d362f9568fea287657
cristian oliveros <3308@holbertonschool.com>
2021-09-13 13:21:24 -0500

    added condition if the string is empty in line 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
     for i in range(1, len(my_list) + 1):
-        print("{}".format(my_list[-1 * i]))
+        print("{:d}".format(my_list[-1 * i]))
