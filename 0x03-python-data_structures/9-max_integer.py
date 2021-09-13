19c55a6727ca94ee20850da61cc2497a8c72f97f
cristian oliveros <3308@holbertonschool.com>
2021-09-13 16:46:38 -0500

    added conditional in line 5

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_num = 0
+
+    if not my_list:
+        return None
+
     for i in range(len(my_list)):
         if my_list[i] >= max_num:
             max_num = my_list[i]
