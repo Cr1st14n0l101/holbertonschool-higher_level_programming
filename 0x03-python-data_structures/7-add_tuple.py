3550e6e079325e108e508cacfef05a2771472ecd
cristian oliveros <3308@holbertonschool.com>
2021-09-13 15:58:37 -0500

    added task 7.

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    num1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    num2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    num3 = tuple_b[0] if len(tuple_b) > 0 else 0
+    num4 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (num1 + num3, num2 + num4)
