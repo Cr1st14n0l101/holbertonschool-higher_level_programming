8aaf27a474dd149aa74d0e23b3a0bf5a3c2f6adc
cristian oliveros <3308@holbertonschool.com>
2021-09-13 22:34:33 -0500

    added task 14.

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p);
+{
+	long int size = PyList_Size(p);
+	int i = 0;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size if the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %i: %s\n", PyTYPE(obj->ob_item[i])->tp_name);
+	}
+}
