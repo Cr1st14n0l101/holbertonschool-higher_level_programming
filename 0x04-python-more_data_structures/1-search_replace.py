06bb8e4dfa02b936b0534e270fd92aba2bf4505c
cristian oliveros <3308@holbertonschool.com>
2021-09-14 12:34:58 -0500

    added tasl 1 - 4

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+ #   new_list = my_list.copy()
+  #  for i in range(len(my_list)):
+   #     if my_list[i] == search:
+    #        new_list[i] = replace
+     #   else:
+      #      new_list[i] = my_list[i]
+    #return new_list
+    return [replace if i == search else i for i in my_list]
