9a9ac4852cb8151d44db6e1aa3f2e73eb6fa1c64
cristian oliveros <3308@holbertonschool.com>
2021-09-27 16:12:24 -0500

    [CREATE]

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    result = 0
+    try:
+        result = fct(args[0], args[1])
+    except ZeroDivisionError as div_zero:
+        print("Exception: {}".format(div_zero), file=sys.stderr)
+        return None
+    except IndexError as out_index:
+        print("Exception: {}".format(out_index), file=sys.stderr)
+        return None
+    return result
