e54def5ec273113033ddba979c092bda2c757712
cristian oliveros <3308@holbertonschool.com>
2021-09-28 17:20:44 -0500

    [CREATE]

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""Create a class Square"""
 class Square:
+    """Make a Square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Create class Square"""
 class Square:
-    """class Square"""
+    """Make a Square"""
 
     def __init__(self, new_size=0):
         self._Square__size = new_size
