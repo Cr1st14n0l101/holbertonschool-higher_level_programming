1691f2203fa6281b5153cd47a937bdf41f47738b
cristian oliveros <3308@holbertonschool.com>
2021-09-28 18:35:12 -0500

    [CREATE]

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
+"""Create a class Square"""
+
+
 class Square:
     "class Square"
+
     def __init__(self, new_size=0):
-        if isinstance(new_size, int):
-            if new_size < 0:
+        self.__size = new_size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
                 raise ValueError("size must be >= 0")
-            self._Square__size = new_size
+            self.__size = value
         else:
             raise TypeError("size must be an integer")
 
     def area(self):
-        self.__area = self.__size * self.__size
+        self.__area = self.__size ** 2
         return self.__area
