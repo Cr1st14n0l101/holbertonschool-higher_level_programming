caf5b412b037da4411e5e7b88f4a46283e75e334
cristian oliveros <3308@holbertonschool.com>
2021-09-28 17:22:46 -0500

    [FIX] added blank spaces

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Create a class Square"""
+
+
 class Square:
     """Make a Square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Create class Square"""
+
+
 class Square:
     """Make a Square"""
 
