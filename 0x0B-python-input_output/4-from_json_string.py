27a1eb68ac1c664f963ce9d5bb68df6b44773343
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-10-12 18:17:26 -0500

    [FIX] spaces at the beggining of the code

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,8 +4,5 @@ import json
 
 
 def from_json_string(my_str):
-    """
-    returns an object (Python data structure) represented by a JSON string
-    """
-
+    """Represent by a JSON string"""
     return json.loads(my_str)
