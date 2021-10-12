9ff03ea458544afbda8e3314cea2c3cd362360d5
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-10-12 17:32:12 -0500

    [FIX] changed line 10

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,7 @@
 def read_file(filename=""):
     """Function that reads a text file and prints it to stdout"""
     with open(filename, 'r', encoding='utf-8') as f:
-        read_data = f.read()
-        print(read_data)
+        for i in f:
+            print(i, end="")
+        print("")
     f.closed
