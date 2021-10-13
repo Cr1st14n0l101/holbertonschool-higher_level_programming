06a0b60005e965b381809a39e0ca796acc228e86
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-10-12 19:39:58 -0500

    [CREATE]

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module for the class Student"""
+
+
+class Student:
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        new_dict = {}
+        if attrs is None:
+            return self.__dict__
+        if isinstance(attrs, list) and all(type(i) == str for i in attrs):
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+
+    def reload_from_json(self, json):
+        """Reload a json file"""
+        for key in json:
+            self.__dict__[key] = json[key]
