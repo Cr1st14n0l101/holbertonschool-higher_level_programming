ce0a19985030c86bf70372a580f250cf789b4bff
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-10-12 22:29:25 -0500

    [CREATE]

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for Class MyIntt"""
+
+
+class MyInt(int):
+    """class MyInt that inherits from int"""
+    def __init__(self, value):
+        """ constructor """
+        self.num = value
+
+    def __eq__(self, value):
+        """ equal change """
+        return int.__ne__(self, value)
+
+    def __ne__(self, value):
+        """ non equal change """
+        return int.__eq__(self, value)
