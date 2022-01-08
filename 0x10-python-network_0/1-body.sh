02e561edade7498a9a0610b7407c18bc236649c8
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-08 17:38:02 -0500

    [FIX] solved only 3 lines per script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Gets the HTTP code and If is 200 then
-# display the body
-if [ "$(curl -sI "$1" | grep '200' | awk '{print $2}')" == '200' ]
-then
-    curl "$1"
-fi
+# Gets the HTTP code and If is 200 then display the body
+if [ "$(curl -sI "$1" | grep '200' | awk '{print $2}')" == '200' ]; then curl "$1"; fi
