7cfc1d5a8ec5ce392e6f263cf2166fc645a19c00
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-08 18:43:22 -0500

    [UPDATED] Removed conditional

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the passed URL, and displays the body of the response
-if [ "$(curl -sLw "%{http_code}" "$1" -o /dev/null)" -eq "200" ]; then curl -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -sLX POST "$1"; fi
+curl -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -sLX POST "$1"
