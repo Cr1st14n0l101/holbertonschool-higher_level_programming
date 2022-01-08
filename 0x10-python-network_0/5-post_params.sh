0b3fb66cd34812a4e6f68b68010f8c46f675009f
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-08 18:44:14 -0500

    [UPDATED]

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the passed URL, and displays the body of the response
-curl -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -sLX POST "$1"
+curl -sLX POST "$1" -H "email: test@gmail.com" -H "subject: I will always be here for PLD" 
