1bcd17a64988e5e0a029661ded6e71b3ef7e5514
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-09 15:42:25 -0500

    [CREATED] added task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL
+and displays the body of
+the response.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        resp = requests.get(argv[1])
+    except requests.exceptions.HTTPError as e:
+        print('Error code:', e.status_code)
