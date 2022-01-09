8d2c787c835099079d581390618638294e12ba82
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-08 22:01:50 -0500

    [UPDATE] pep8 style?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import urllib.request
 """Fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
 
 if __name__ == "__main__":
+
     url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         html = response.read()
