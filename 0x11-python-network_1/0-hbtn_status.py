968ca00d1c1af851af5d43a9f22e485697184622
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-09 12:09:22 -0500

    [UPDATE] the code works but says that No

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
