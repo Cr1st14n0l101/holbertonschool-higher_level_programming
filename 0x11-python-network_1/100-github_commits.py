29c052759c6341f56b0c0a45eaa4d5de049ac7bb
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-09 21:51:47 -0500

    [CREATED] added task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     resp = requests.get(url)
     json_repr = resp.json()
-    for i in range(0, 10):
+
+    for i in range(10 if len(json_repr) >= 10 else len(json_repr)):
         sha = json_repr[i].get('sha')
         author_name = json_repr[i].get('commit').get('author').get('name')
         print("{}: {}".format(sha, author_name))
