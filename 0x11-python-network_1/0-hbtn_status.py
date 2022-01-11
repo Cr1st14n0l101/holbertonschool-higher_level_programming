c8662c4e172e38e9f7c776e51dd8bb7210b88cdd
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-01-11 09:53:20 -0500

    [UPDATED] removed tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
-        print('    - type:', type(html))
-        print('    - content:', html)
-        print('    - utf8 content:', html.decode('UTF-8'))
+        print('- type:', type(html))
+        print('- content:', html)
+        print('- utf8 content:', html.decode('UTF-8'))
