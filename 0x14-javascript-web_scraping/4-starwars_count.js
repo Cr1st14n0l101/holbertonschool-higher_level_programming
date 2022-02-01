1b02e78b5d1828f38308d44bf0a72fcf7c92d528
Cristian Oliveros <wolfmanoliveros@gmail.com>
2022-02-01 13:37:59 -0500

    [ADDED] Added task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const url = args[2];
+const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+let counter = 0;
+request(url, function (error, response, body) {
+  if (error) throw error;
+  const json = JSON.parse(body);
+  const size = Object.keys(json.results).length;
+  for (let i = 0; i < size; i++) {
+    const sizeCarasters = Object.keys(json.results[i].characters).length;
+    for (let j = 0; j < sizeCarasters; j++) {
+      if (json.results[i].characters[j] === characterUrl) {
+        counter++;
+      }
+    }
+  }
+  console.log(counter);
+});
